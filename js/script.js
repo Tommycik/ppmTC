26aef9840c94dd67a5d1149016d9613a8c6bb224
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-05-01 11:24:54 +0200

    7.0

--- js/script.js
+++ js/script.js
@@ -11,3 +11,27 @@ function elementVisible(id,n){
   }
 
 }
+
+function showMore(){
+  document.getElementsByClassName("showMore")[0].style.display="none";
+  document.getElementById("lastSub").style.border="none";
+  document.getElementsByClassName("hiddenArticles")[0].style.display="flex";
+  document.getElementsByClassName("centeredGeometry")[0].style.display="none";
+}
+function menu1(){
+  var x= document.getElementById("lateralMenu");
+
+  if(visible[0]){
+    document.getElementById("menu1").src="img/placeholder.svg";
+    x.style.backgroundColor="rgba(0, 0, 0, 0)";
+    document.getElementById("body").style.overflow;
+
+  }else{
+    document.getElementById("menu1").src="img/placeholder.svg";
+    x.style.backgroundColor="rgba(0, 0, 0, 0.5)"
+    document.getElementById("body").style.overflow="hidden";
+  }
+  elementVisible('lateralMenu',0);
+
+
+}
